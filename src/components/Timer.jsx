ee8a51dd17670901be970318a769a72cf4fbb156
George Olufemi <93535171+George-Olufemi@users.noreply.github.com>
2022-06-27 06:14:30 +0100

    Update Timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -10,7 +10,7 @@ const Timer = () => {
   let interval = useRef();
 
   const startTimer = () => {
-    const countdownDate = new Date('June 26 2022 00:00:00').getTime();
+    const countdownDate = new Date('June 29 2022 00:00:00').getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
